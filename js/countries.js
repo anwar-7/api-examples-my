9929532cc30ce1debd9c1c792ce77fd90a6d8414
anwarul1 <anwarjewel007@gmail.com>
2022-02-24 21:51:17 +0600

    my one

--- js/countries.js
+++ js/countries.js
@@ -34,17 +34,49 @@ lodeCountries();
 const displayCountries = (countries) => {
   const countriesDiv = document.getElementById('countries');
   countries.forEach((country) => {
-    console.log(country);
-    const div = document.createElement('div');
-    div.className = 'col-md-4';
-    const h4 = document.createElement('h4');
-    h4.innerText = `Name: ${country.name.common}`;
-    div.appendChild(h4);
-    const p = document.createElement('p');
-    p.innerText = `Official Name: ${country.name.official}`;
-    div.appendChild(p);
     // console.log(country);
-    // main append
+    const div = document.createElement('div');
+    div.className = 'country';
+    div.innerHTML = `
+    <h3>Name: ${country.name.common}</h3>
+    <p>Official Name: ${country.name.official}</p>
+    <button onclick="lodeCountryDetails('${country.name.common}')">Details</button>
+    `;
     countriesDiv.appendChild(div);
   });
 };
+// my work
+const lodeCountryDetails = (name) => {
+  const url = `https://restcountries.com/v3.1/name/${name}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayCountryDetails(data[0]));
+};
+
+const displayCountryDetails = (country) => {
+  const countriesDetailsDiv = document.getElementById('countries-details');
+  countriesDetailsDiv.innerHTML = `
+    <h5>Country Details</h5>
+  <p>Population: ${country.population}</p>
+  <img width="200px" src="${country.flags.png}">
+    `;
+};
+
+/* 
+const lodeCountryByName = (name) => {
+  const url = `https://restcountries.com/v3.1/name/${name}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => displayCountryByName(data[0]));
+};
+
+const displayCountryByName = (country) => {
+  //   console.log(country);
+  const countryDiv = document.getElementById('countries-details');
+  countryDiv.innerHTML = `
+  <h5>Country Details</h5>
+  <p>Population: ${country.population}</p>
+  <img width="200px" src="${country.flags.png}">
+  `;
+};
+ */
