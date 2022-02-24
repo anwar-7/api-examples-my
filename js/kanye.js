d8346891a805b1710ff995e072fc66948717cc4e
anwarul1 <anwarjewel007@gmail.com>
2022-02-24 17:22:00 +0600

    my update commit

--- js/kanye.js
+++ js/kanye.js
@@ -1,3 +1,4 @@
+/* 
 const lodeQuote = () => {
   fetch('https://api.kanye.rest/')
     .then((response) => response.json())
@@ -8,4 +9,17 @@ const displayQuote = (quote) => {
   const quoteElement = document.getElementById('quote');
   quoteElement.innerText = quote.quote;
   //   console.log(quote);
+};
+*/
+
+const lodeQuote = () => {
+  fetch('https://api.kanye.rest/')
+    .then((response) => response.json())
+    .then((data) => displayQuote(data));
+};
+
+const displayQuote = (quote) => {
+  const quoteElement = document.getElementById('quote');
+  quoteElement.innerText = quote.quote;
+  //   console.log(quote.quote);
 };
